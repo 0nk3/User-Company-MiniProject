69ad90e38ba1742c36c41b20db88833d317e4c29
0nk3 <0nk3.0nk3@gmail.com>
2020-07-12 00:12:04 +0200

    random user and random company mini project in TypeScript . . .

---
+++ src/CustomMap.ts
@@ -0,0 +1,41 @@
+import { } from "google-maps";
+
+interface Mappable{
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(id: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(id), {
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+      zoom: 1,
+    });
+  }
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+  // add event listener on the marker
+    marker.addListener('click', () => {
+      const info = new google.maps.InfoWindow({
+        content: 'Holla!'
+      });
+      info.open(this.googleMap, marker);
+    });
+  
+  }
+
+}
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { CustomMap } from "./CustomMap";
+import { Company } from "./Company";
+import { User } from "./User";
+
+const user = new User();
+const company = new Company();
+console.log(user);
+console.log(company);
+
+const customMap = new CustomMap("map");
+customMap.addMarker(user);
+customMap.addMarker(company);
